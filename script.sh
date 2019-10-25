c6802a0a8f1987eea0b6aa9630b96d866d10aa8d
Madhav Sodhani <madhav2302@gmail.com>
2019-10-25 15:05:59 -0400

    update script and result

--- script.sh
+++ script.sh
@@ -1,36 +1,54 @@
 echo "Number of nodes 100"
+echo ""
 time mix run project3.exs 100 10
 echo "Number of nodes 200"
+echo ""
 time mix run project3.exs 200 10
 echo "Number of nodes 300"
+echo ""
 time mix run project3.exs 300 10
 echo "Number of nodes 400"
+echo ""
 time mix run project3.exs 400 10
 echo "Number of nodes 500"
+echo ""
 time mix run project3.exs 500 10
 echo "Number of nodes 600"
+echo ""
 time mix run project3.exs 600 10
 echo "Number of nodes 700"
+echo ""
 time mix run project3.exs 700 10
 echo "Number of nodes 800"
+echo ""
 time mix run project3.exs 800 10
 echo "Number of nodes 900"
+echo ""
 time mix run project3.exs 900 10
 echo "Number of nodes 1000"
+echo ""
 time mix run project3.exs 1000 10
 echo "Number of nodes 1500"
+echo ""
 time mix run project3.exs 1500 10
 echo "Number of nodes 2000"
+echo ""
 time mix run project3.exs 2000 10
-# echo "Number of nodes 2500"
-# time mix run project3.exs 2500 10
-# echo "Number of nodes 3000"
-# time mix run project3.exs 3000 10
-# echo "Number of nodes 3500"
-# time mix run project3.exs 3500 10
-# echo "Number of nodes 4000"
-# time mix run project3.exs 4000 10
-# echo "Number of nodes 4500"
-# time mix run project3.exs 4500 10
-# echo "Number of nodes 5000"
-# time mix run project3.exs 5000 10
+echo "Number of nodes 2500"
+echo ""
+time mix run project3.exs 2500 10
+echo "Number of nodes 3000"
+echo ""
+time mix run project3.exs 3000 10
+echo "Number of nodes 3500"
+echo ""
+time mix run project3.exs 3500 10
+echo "Number of nodes 4000"
+echo ""
+time mix run project3.exs 4000 10
+echo "Number of nodes 4500"
+echo ""
+time mix run project3.exs 4500 10
+echo "Number of nodes 5000"
+echo ""
+time mix run project3.exs 5000 10
